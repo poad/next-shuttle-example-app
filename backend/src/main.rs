6389fcb361869b8877a81e570779de83136a7787
Kenji Saito <ken-yo@mbr.nifty.com>
2023-07-09 00:18:25 +0900

    Bumps axum-extra

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,19 @@
 use std::path::PathBuf;
 
 use axum::Router;
-use axum_extra::routing::SpaRouter;
+use tower_http::{
+    services::{ServeDir, ServeFile}
+};
 
 #[shuttle_runtime::main]
 async fn axum(
     #[shuttle_static_folder::StaticFolder] static_folder: PathBuf,
 ) -> shuttle_axum::ShuttleAxum {
+    let static_path = format!("{}/index.html", static_folder.into_os_string().into_string().unwrap());
     let router =
-        Router::new().merge(SpaRouter::new("/", static_folder).index_file("index.html"));
+    Router::new().nest_service(
+            "/",
+            ServeDir::new("/").not_found_service(ServeFile::new(static_path)));
 
     Ok(router.into())
 }
